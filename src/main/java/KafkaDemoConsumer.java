553a358dcaf80603b2778f51fc27ce898d4f71ec
T903509 <bibhuti.chakraborty1@bellmedia.ca>
2023-04-02 08:57:30 +0530

    Demo

---
+++ src/main/java/KafkaDemoConsumer.java
@@ -0,0 +1,39 @@
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+
+public class KafkaDemoConsumer {
+    public static void main(String[] args) {
+        Logger logger= LoggerFactory.getLogger(KafkaDemoConsumer.class.getName());
+        String bootstrapServers="127.0.0.1:9092";
+        String grp_id="third_app";
+        String topic="my_first";
+        //Creating consumer properties
+        Properties properties=new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,   StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,grp_id);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        //creating consumer
+        KafkaConsumer<String,String> consumer= new KafkaConsumer<String,String>(properties);
+        //Subscribing
+        consumer.subscribe(Arrays.asList(topic));
+        //polling
+        while(true){
+            ConsumerRecords<String,String> records=consumer.poll(Duration.ofMillis(100));
+            for(ConsumerRecord<String,String> record: records){
+                logger.info("Key: "+ record.key() + ", Value:" +record.value());
+                logger.info("Partition:" + record.partition()+",Offset:"+record.offset());
+            }
+
+
+        }
+    }
+}
